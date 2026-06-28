8a88e8f10469d4a29499abdca3146751da08919e
Mctraxer228 <cernovdaniil@seniorpro.com>
2026-06-28 21:25:53 +0700

    jijnja2 integrate

---
+++ api/routers/words_web.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, BackgroundTasks
+from fastapi.responses import FileResponse
+from asyncio import get_running_loop
+from schemas.words import CreateWord, Word, UpdateWord
+from services.words import word_service
+from api.depends.session_dep import SessionDep
+from utils_backend.export_anki import export_anki
+from utils_backend.remove_file import remove_file
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter(include_in_schema=False) 
+templates = Jinja2Templates(directory="templates")
+
+@router.get('/')
+async def get_all_words(session: SessionDep) -> list[Word]:
+    words = await word_service.get_all_words(session)
+    return [Word.model_validate(i) for i in words]
+
+@router.get('/{id}')
+async def get_by_id(session: SessionDep, id: int) -> list[Word]:
+    words = await word_service.get_word_by_id(session, id)
+    return [Word.model_validate(i) for i in words]
+
+@router.post('/', response_model = Word)
+async def create_words(session: SessionDep, word_scheme: CreateWord) -> Word:
+    word = await word_service.create_word(session, word_scheme)
+    return Word.model_validate(word)
+    
+@router.delete('/{id}')
+async def delete_word(session: SessionDep, id: int):
+    status = await word_service.delete_word(session, id)
+    return status
+    
+@router.patch('/{id}')
+async def update_word(session: SessionDep, id: int, update_scheme: UpdateWord) -> Word:
+    updated_word = await word_service.update_word(session, id, update_scheme)
+    return Word.model_validate(updated_word)
+    
+    
+@router.post('/export_anki')
+async def export_words(session: SessionDep, background_tasks: BackgroundTasks):
+    words = await word_service.get_all_words(session)
+    words_list = [
+        Word.model_validate(w).model_dump() for w in words
+    ]
+    loop = get_running_loop()
+    output_path = await loop.run_in_executor(None, export_anki, words_list)
+    background_tasks.append(remove_file(str(output_path)))
+    return FileResponse(
+        output_path,
+        filename="anki_cards.apkg", 
+        media_type="application/octet-stream"
+    )
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import uvicorn
 from config import settings
 from contextlib import asynccontextmanager
 from database.session import create_db
-from api.routers.words import router as words_router
+from api.routers.words_api import router as words_api_router
+from api.routers.words_web import router as words_web_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,7 +17,8 @@ app = FastAPI(lifespan=lifespan, )
 async def health_check():
     return {'status': 'work!'}
 
-app.include_router(words_router, tags=['Words'])
+app.include_router(words_api_router, tags=['Words'])
+app.include_router(words_web_router)
 
 if __name__ == "__main__":
     uvicorn.run('main:app', reload=settings.RELOAD, port=settings.PORT, host=settings.HOST)
